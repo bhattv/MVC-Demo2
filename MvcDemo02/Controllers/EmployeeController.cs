1be518cb6b0f7417481611e6f26324d9bf328756
bhattv <bhattv@gmail.com>
2014-11-07 21:00:11 -0500

    initial checkin

---
+++ MvcDemo02/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer1;
+
+namespace MvcDemo02.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+
+            EmployeeBusinessLayer employeeBusinessLayer =
+           new EmployeeBusinessLayer();
+
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+            return View(employees);
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            Employee employee = new Employee();
+            // Retrieve form data using form collection
+            employee.Name = formCollection["Name"];
+            employee.Gender = formCollection["Gender"];
+            employee.City = formCollection["City"];
+            employee.DateOfBirth =
+                Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+            EmployeeBusinessLayer employeeBusinessLayer =
+                new EmployeeBusinessLayer();
+
+            employeeBusinessLayer.AddEmmployee(employee);
+            return RedirectToAction("Index");
+            //foreach (string key in formCollection.AllKeys)
+            //{
+            //    Response.Write("Key = " + key + "  ");
+            //    Response.Write(formCollection[key]);
+            //    Response.Write("<br/>");
+            //}
+
+            //return View();
+        }
+
+
+    }
+}
---
+++ MvcDemo02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcDemo02.MvcApplication" Language="C#" %>
